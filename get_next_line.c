e2852c22667f4170474343f619a3b1f87871ef30
ntenisha <91886143+ntenisha@users.noreply.github.com>
2022-01-07 18:48:14 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-char *cft_check_rem(char *rem, char **line)
+char *check_rem(char *rem, char **line)
 {
     char    *p_n;
 
@@ -22,10 +22,29 @@ char *cft_check_rem(char *rem, char **line)
 	return(p_n);
 }
 
-char *get_next_line(int fd)
+char *get_next_line(int fd, char **line)
 {
+    static char     *rem;
+    char            buff[BUFFER_SIZE + 1];
+    int             bwr;
+    char            *p_n;
+    char            tmp;
 
-
+    p_n = check_rem(rem, line);
+    while (!p_n && (bwr = read  (fd, buf, BUFFER_SIZE)))
+    {
+        buf[bwr] = '\0';
+        if ((p_n = strchr(buf, '\n')))
+        {
+            *p_n = '\0';
+            p_n++;
+            rem = ft_strdup(p_n);
+        }
+        tmp = *line;
+        *line = ft_strjoin(*line, buf);
+        free(tmp);
+    }
+    return (bwr || ft_strlen(rem) || ft_strlen(*line)) ? 1 : 0;
 }
 
 int main (void)
@@ -36,6 +55,4 @@ int main (void)
     fd = open ("text.txt", O_RDONLY);
     while (get_next_line(fd, &line))
         printf("%s \n\n , line");
-    
-
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-#define BUFFER_SIZE=42;
+#define BUFFER_SIZE 42;
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *src);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *str, int ch);
 
+char    *check_rem(char *rem, char **line);
 char    *get_next_line(int fd);
 
 #endif
